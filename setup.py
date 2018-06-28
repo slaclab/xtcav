41f30b2ee2e5baadd69567800f9854016b90e587
tjlane <thomas.joseph.lane@gmail.com>
2018-06-28 12:06:20 -0700

    fix xtcav2 path issue in setup.py

--- setup.py
+++ setup.py
@@ -4,5 +4,6 @@ setup(name='xtcav2',
       version='0.1',
       description='Updated XTCAV analysis code',
       packages=['xtcav2'],
+      package_dir = {'xtcav2': 'xtcav'},
       scripts=['bin/xtcavDisplay', 'bin/xtcavDark', 'bin/xtcavLasingOff', 'bin/xtcavLasingOn'])
 
